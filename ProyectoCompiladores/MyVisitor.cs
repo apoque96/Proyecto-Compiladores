a89773463d0b290813ee4019f2782700b4ac38b9
sofia <sofia12.to@gmail.com>
2025-04-13 19:08:09 -0600

    MyVisitor, Implementandos los próximos 10 No Terminales y sus respectivos nodos de AST

--- ProyectoCompiladores/MyVisitor.cs
+++ ProyectoCompiladores/MyVisitor.cs
@@ -13,8 +13,8 @@ public class MyVisitor : GramaticaBaseVisitor<object>
 
     public override object VisitPg(GramaticaParser.PgContext context)
     {
-        Console.WriteLine("Visitando Pg: " + context.GetText()); 
-        var pgNode = new AST.PgNode();  
+        Console.WriteLine("Visitando Pg: " + context.GetText());
+        var pgNode = new AST.PgNode();
         return base.VisitPg(context);
     }
 
@@ -39,5 +39,75 @@ public class MyVisitor : GramaticaBaseVisitor<object>
         return base.VisitD(context);
     }
 
+    public override object VisitE(GramaticaParser.EContext context)
+    {
+        Console.WriteLine("Visitando E: " + context.GetText());
+        var eNode = new AST.ENode();
+        return base.VisitE(context);
+    }
 
+    public override object VisitEp(GramaticaParser.EpContext context)
+    {
+        Console.WriteLine("Visitando Ep: " + context.GetText());
+        var epNode = new AST.EpNode();
+        return base.VisitEp(context);
+    }
+
+    public override object VisitT(GramaticaParser.TContext context)
+    {
+        Console.WriteLine("Visitando T: " + context.GetText());
+        var tNode = new AST.TNode();
+        return base.VisitT(context);
+    }
+
+    public override object VisitTp(GramaticaParser.TpContext context)
+    {
+        Console.WriteLine("Visitando Tp: " + context.GetText());
+        var tpNode = new AST.TpNode();
+        return base.VisitTp(context);
+    }
+
+    public override object VisitF(GramaticaParser.FContext context)
+    {
+        Console.WriteLine("Visitando F: " + context.GetText());
+        var fNode = new AST.FNode();
+        return base.VisitF(context);
+    }
+
+    public override object VisitDs(GramaticaParser.DsContext context)
+    {
+        Console.WriteLine("Visitando Ds: " + context.GetText());
+        var dsNode = new AST.DsNode();
+        return base.VisitDs(context);
+    }
+
+    public override object VisitDsp(GramaticaParser.DspContext context)
+    {
+        Console.WriteLine("Visitando Dsp: " + context.GetText());
+        var dspNode = new AST.DspNode();
+        return base.VisitDsp(context);
+    }
+
+    public override object VisitIo(GramaticaParser.IoContext context)
+    {
+        Console.WriteLine("Visitando Io: " + context.GetText());
+        var ioNode = new AST.IoNode();
+        return base.VisitIo(context);
+    }
+
+    public override object VisitCe(GramaticaParser.CeContext context)
+    {
+        Console.WriteLine("Visitando Ce: " + context.GetText());
+        var ceNode = new AST.CeNode();
+        return base.VisitCe(context);
+    }
+
+    public override object VisitIfd(GramaticaParser.IfdContext context)
+    {
+        Console.WriteLine("Visitando Ifd: " + context.GetText());
+        var ifdNode = new AST.IfdNode();
+        return base.VisitIfd(context);
+    }
 }
+
+   
